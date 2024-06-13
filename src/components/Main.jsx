06ae94cce543394b74c0fc5cc05528c87730e30e
Alethepata <alessia.patane@gmail.com>
2024-06-13 14:44:23 +0200

    edit card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,23 @@
 import Card from "./partials/Card"
+import { posts } from '../db/posts.js';
 
 function Main() {
     return (
         <main>
             <div className="cards">
-                <Card/>
+                {
+                    posts.map(post => (
+                        post.published &&
+                            <Card
+                                key={`post_${post.id}`}
+                                image={post.image}
+                                title={post.title}
+                                content={post.content}
+                                tags={post.tags}
+                                published={post.published}
+                            />
+                    ))
+                }
             </div>
         </main>
     )
--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -1,24 +1,29 @@
-import img from '../../assets/img/img-post.jpg';
-
-function Card() {
+function Card({image, title, content, tags, published }) {
     return (
-        <div className="card">
-            <figure>
-                <img src={img} alt=""/>
-            </figure>
-            <div className="card-content">
-                <div className="card-text">
-                    <h3>
-                        Titolo del Post
-                    </h3>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil necessitatibus beatae, veniam fuga similique illo iure itaque dolorem rem sed, aspernatur voluptatibus voluptates? Vel cum dolorum, aliquam tempora possimus modi quos unde. Nulla unde ipsa voluptatem facere accusamus delectus aliquid enim rem rerum in recusandae consequatur, dolore laborum distinctio consectetur eius iusto placeat. Omnis fugit, pariatur atque laboriosam doloremque itaque.
-                    </p>
-                </div>
-                <div className="card-action">
-                    <button className="button">Leggi di più</button>
-                </div>
+    <div className = "card" >
+        <figure>
+            {
+                image != '' ? <img src={image} alt=""/> : <img src="https://placehold.co/600x400" alt=""/>
+            }
+        </figure>
+        <div className="card-content">
+            <div className="card-text">
+                <h3>
+                    {title}
+                </h3>
+                <p>
+                    {content}
+                </p>
+                {
+                    tags.map((tag, index) => (
+                        <span className={`tag tag_${tag}`} key={`tag_${index}`}>#{tag}</span>
+                    ))
+                }
             </div>
+            <div className="card-action">
+                <button className="button">Leggi di più</button>
+            </div>
+        </div>
 
         </div>
     )
